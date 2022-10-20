644e3b4c5a2f6b7293f6528d91f88ed5fe8c8941
ret7020 <ret7020@gmail.com>
2022-10-20 19:20:54 +0300

    Appartaments block finished

--- src/Components/ApartamentItem.jsx
+++ src/Components/ApartamentItem.jsx
@@ -2,21 +2,26 @@ import React from "react";
 import 'bootstrap/dist/css/bootstrap.min.css';
 import Card from 'react-bootstrap/Card';
 import Col from 'react-bootstrap/Col';
+import Button from 'react-bootstrap/Button';
 import "./Appartaments.css";
 
 
 const AppartamentItem = ({ flat_data, ...props }) => {
     return (
         <Col>
-            <Card style={{ width: '270px', marginBottom: '30px', borderColor: '#fff', borderRadius: '4px'}}>
+            <Card className="appartament_item">
                 <Card.Img variant="top" src={require("../img/photo.jpg")} alt="Appartament" />
+                <Card.ImgOverlay>
+                    <Button className="image_filter_overlay_btn" variant="warning" size="sm">Комфорт</Button>
+                    <Button className="image_filter_overlay_btn" variant="success" size="sm">Рассрочка</Button>
+                </Card.ImgOverlay>
                 <Card.Body>
-                    <Card.Title style={{"fontWeight": "700"}}>{flat_data.title}</Card.Title>
+                    <Card.Title style={{ "fontWeight": "700" }}>{flat_data.title}</Card.Title>
                     <Card.Text>
-                        <div style={{"display": "grid"}}>
+                        <div style={{ "display": "grid" }}>
                             <span className="release_date_text">Срок сдачи до {flat_data.release_quarter} кв. {flat_data.release_year} г.</span>
                             <div>
-                                <img src={require("../img/metro.png")} className="metro_icon" alt="Moscow metro"/>
+                                <img src={require("../img/metro.png")} className="metro_icon" alt="Moscow metro" />
                                 <span className="metro_station_name">{flat_data.metro_station}</span>
                             </div>
                             <span className="address_text">{flat_data.address}</span>
